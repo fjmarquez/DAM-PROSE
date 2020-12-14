4e244909f2bbc8bfcca8a667746ebca001f66235
Francisco José Márquez Pulido <43822565+fjmarquez@users.noreply.github.com>
2020-12-14 23:12:50 +0100

    Repaso Examen PROSE

---
+++ ServiciosWeb/ejemploRestMVC/model/ConsValoracionesModel.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace ConstantesDB;
+
+//Class with constants for libros table.
+class ConsValoracionesModel
+{
+    const TABLE_NAME = "ratings";
+    const IDUSER = "IDUser";
+    const IDPRODUCT = "IDProduct";
+    const RATING = "Rating";
+    const REVIEW = "Review";
+    
+}
